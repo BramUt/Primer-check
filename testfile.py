1a8b01a408e420c1804ac0bdaea56428e1d405eb
BramUt <bram.utberg@gmail.com>
2019-11-21 14:40:33 +0100

    Now addigns variables

--- testfile.py
+++ testfile.py
@@ -2,45 +2,47 @@ from tkinter import *
 from tkinter import scrolledtext, messagebox
 
 
-def gui():
+class Gui:
 
-    def clicked_5():
-        raw_seq_5 = txt_5.get("1.0", END)
-        txt_5.configure(state='disabled', bg="light grey")
-        return raw_seq_5
+    def __init__(self):
+        def clicked_5():
+            self.raw_seq_5 = txt_5.get("1.0", END)
+            txt_5.configure(state='disabled', bg="light grey")
+            return raw_seq_5
 
-    def clicked_3():
-        raw_seq_3 = txt_3.get("1.0", END)
-        txt_3.configure(state="disabled", bg="light grey")
-        return raw_seq_3
+        def clicked_3():
+            raw_seq_3 = txt_3.get("1.0", END)
+            txt_3.configure(state="disabled", bg="light grey")
+            return raw_seq_3
 
-    window = Tk()
-    window.title("Primer check 2")
-    window.geometry("750x750")
+        window = Tk()
+        window.title("Primer check 2")
+        window.geometry("750x500")
 
-    label_5 = Label(window, text="Enter 5'-UTR")
-    txt_5 = scrolledtext.ScrolledText(window, width=40, height=20)
-    button_5 = Button(window, text="OK",
-                      command=lambda: raw_seq_5 == clicked_5())
-    label_5.grid(column=0, row=0, sticky=W)
-    txt_5.grid(column=0, row=1)
-    button_5.grid(column=0, row=2, sticky=E)
-    txt_5.focus()
+        raw_seq_5 ="Nee"
 
-    label_3 = Label(window, text="Enter 3'-UTR")
-    txt_3 = scrolledtext.ScrolledText(window, width=40, height=20)
-    button_3 = Button(window, text="OK", command=clicked_3)
-    label_3.grid(column=3, row=0, sticky=W)
-    txt_3.grid(column=3, row=1)
-    button_3.grid(column=3, row=2, sticky=E)
+        label_5 = Label(window, text="Enter 5'-UTR")
+        txt_5 = scrolledtext.ScrolledText(window, width=40, height=20)
+        button_5 = Button(window, text="OK", state='active',
+                          command=clicked_5)
+        label_5.grid(column=0, row=0, sticky=W)
+        txt_5.grid(column=0, row=1)
+        button_5.grid(column=0, row=2, sticky=E)
+        txt_5.focus()
 
-    exit_button = Button(window,text="Quit", command=window.quit)
-    exit_button.grid(column=3, row=4, sticky=E)
+        label_3 = Label(window, text="Enter 3'-UTR")
+        txt_3 = scrolledtext.ScrolledText(window, width=40, height=20)
+        button_3 = Button(window, text="OK", command=clicked_3)
+        label_3.grid(column=3, row=0, sticky=W, padx=10)
+        txt_3.grid(column=3, row=1, padx=10)
+        button_3.grid(column=3, row=2, sticky=E, padx=10)
 
-    window.mainloop()
+        exit_button = Button(window,text="Quit", command=window.quit)
+        exit_button.grid(column=3, row=4, sticky=E)
 
-    print(raw_seq_5)
-    print(raw_seq_3)
+        window.mainloop()
+
+        print(self.raw_seq_5)
 
 
-gui()
+Gui()
