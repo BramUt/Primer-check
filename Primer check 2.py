fb49942cba38185739dd60d1c1c70ab034da427f
BramUt <bram.utberg@gmail.com>
2019-11-21 15:59:41 +0100

    Added seq stove

--- Primer check 2.py
+++ Primer check 2.py
@@ -3,24 +3,25 @@ from tkinter import scrolledtext, messagebox
 import re
 
 
-class Gui:
+class InputGui:
 
     def __init__(self):
         def clicked_5():
             self.raw_seq_5 = txt_5.get("1.0", END)
-            txt_5.configure(state='disabled', bg="light grey")
+            txt_5.configure(state="disabled", bg="light grey")
 
         def clicked_3():
             self.raw_seq_3 = txt_3.get("1.0", END)
             txt_3.configure(state="disabled", bg="light grey")
+            exit_button.configure(state="active")
 
         window = Tk()
         window.title("Primer check 2")
-        window.geometry("750x500")
+        window.geometry("700x430")
 
         label_5 = Label(window, text="Enter 5'-UTR")
         txt_5 = scrolledtext.ScrolledText(window, width=40, height=20)
-        button_5 = Button(window, text="OK", command=clicked_5)
+        button_5 = Button(window, text="Enter", command=clicked_5)
         label_5.grid(column=0, row=0, sticky=W)
         txt_5.grid(column=0, row=1)
         button_5.grid(column=0, row=2, sticky=E)
@@ -28,20 +29,20 @@ class Gui:
 
         label_3 = Label(window, text="Enter 3'-UTR")
         txt_3 = scrolledtext.ScrolledText(window, width=40, height=20)
-        button_3 = Button(window, text="OK", command=clicked_3)
+        button_3 = Button(window, text="Enter", command=clicked_3)
         label_3.grid(column=3, row=0, sticky=W, padx=10)
         txt_3.grid(column=3, row=1, padx=10)
         button_3.grid(column=3, row=2, sticky=E, padx=10)
 
-        exit_button = Button(window,text="Quit", command=window.quit)
+        exit_button = Button(window, text="Continue", command=window.quit,
+                             state="disabled")
         exit_button.grid(column=3, row=4, sticky=E)
 
         window.mainloop()
 
 
 def fun_return():
-    print("check")
-    return Gui()
+    return InputGui()
 
 
 def seq_stove(raw_seq):
@@ -52,7 +53,7 @@ def seq_stove(raw_seq):
     Output: rare_seq - str, DNA/RNA sequence without numbers or whitespaces
     """
 
-    med_seq = raw_seq.replace(" ", "")
+    med_seq = raw_seq.replace(" ", "").replace("\n", "")
 
     rare_seq = re.sub(r'[0-9]+', "", med_seq)
 
@@ -131,10 +132,13 @@ def main():
 
     pre_seq_3, pre_seq_5 = res.raw_seq_3, res.raw_seq_5
 
-    print("Check", pre_seq_3)
+    seq_5 = seq_stove(pre_seq_5).upper()
 
-    seq_5 = seq_stove(pre_seq_5)
-    seq_3 = seq_stove(pre_seq_3)
+    med_seq_3 = seq_stove(pre_seq_3).upper()
+    seq_3 = complementing_strand(med_seq_3)
+
+    print(med_seq_3)
+    print(seq_3)
 
     print(seq_5)
 
