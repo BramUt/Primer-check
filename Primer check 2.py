2d32cc7f84f291426f4c1513feebc4866cf973da
BramUt <bram.utberg@gmail.com>
2019-11-21 17:21:22 +0100

    Added second UI

--- Primer check 2.py
+++ Primer check 2.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from tkinter import scrolledtext, messagebox
+from tkinter.ttk import *
+from tkinter import scrolledtext
 import re
 
 
@@ -43,6 +44,76 @@ class InputGui:
 
 def fun_return():
     return InputGui()
+
+
+def gui_2():
+
+    window = Tk()
+    window.title("Primer check 2: parameter")
+    window.geometry("250x175")
+
+    var_1, var_2, var_3, var_4 = IntVar(), IntVar(), IntVar(), IntVar()
+    var_1.set(60)
+    var_2.set(40)
+    var_3.set(55)
+    var_4.set(62)
+
+    label_maxcg = Label(window, text="Max CG%", font=("Arial", 12))
+    selector_maxcg = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_1, font=("Arial", 12))
+    label_maxcg.grid(column=0, row=0, sticky=W)
+    selector_maxcg.grid(column=0, row=1, sticky=W)
+    selector_maxcg.focus()
+
+    label_mincg = Label(window, text="Min CG%", font=("Arial", 12))
+    selector_mincg = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_2, font=("Arial", 12))
+    label_mincg.grid(column=0, row=2, sticky=W)
+    selector_mincg.grid(column=0, row=3, sticky=W)
+
+    label_maxtm = Label(window, text="Max Tm", font=("Arial", 12))
+    selector_maxtm = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_3, font=("Arial", 12))
+    label_maxtm.grid(column=0, row=4, sticky=W)
+    selector_maxtm.grid(column=0, row=5, sticky=W)
+
+    label_mintm = Label(window, text="Min Tm", font=("Arial", 12))
+    selector_mintm = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_4, font=("Arial", 12))
+    label_mintm.grid(column=0, row=6, sticky=W)
+    selector_mintm.grid(column=0, row=7, sticky=W)
+
+    label_maxlen = Label(window, text="Max primer length", font=("Arial", 12))
+    selector_maxlen = Spinbox(window, from_=0, to=100, width=5,
+                              textvariable=var_3, font=("Arial", 12))
+    label_maxlen.grid(column=0, row=4, sticky=W)
+    selector_maxlen.grid(column=0, row=5, sticky=W)
+
+    label_minlen = Label(window, text="Min primer length", font=("Arial", 12))
+    selector_minlen = Spinbox(window, from_=0, to=100, width=5,
+                              textvariable=var_4, font=("Arial", 12))
+    label_minlen.grid(column=0, row=6, sticky=W)
+    selector_minlen.grid(column=0, row=7, sticky=W)
+
+    continue_button = Button(window, text="Continue",
+                             command=lambda: window.quit())
+    continue_button.grid(column=2, row=7, sticky=E)
+
+    window.mainloop()
+
+    max_cg = selector_maxcg.get()
+    min_cg = selector_mincg.get()
+    max_tm = selector_maxtm.get()
+    min_tm = selector_mintm.get()
+    max_len = selector_maxlen.get()
+    min_len = selector_minlen.get()
+
+    return [max_cg, min_cg, max_tm, min_tm, max_len, min_len]
+
+
+
+def primer_searcher(utr):
+    pass
 
 
 def seq_stove(raw_seq):
@@ -97,8 +168,9 @@ def seq_info(seq_list):
         cg_count = seq.count("C") + seq.count("G")
         cg_perc = round((cg_count / len(seq)*100),2)
         smelt_temp = ((at_count*2) + (cg_count*4))
+        length = len(seq)
 
-        temp_list = [seq, cg_count, at_count, cg_perc, smelt_temp]
+        temp_list = [seq, cg_count, at_count, cg_perc, smelt_temp, length]
 
         info_list.append(list(map(str, temp_list)))
 
@@ -113,13 +185,13 @@ def file_writer(forward_list, reverse_list):
     """
 
     with open("Resultaten.csv", "w") as res_file:
-        kopje = str("Forward primers\nSeq 5'-3',CG,AT,CG%,Smelt temp\n")
+        kopje = "Forward primers\nSeq 5'-3',CG,AT,CG%,Smelt temp,Length\n"
         res_file.write(kopje)
         for x in forward_list:
             temp_string = str(",".join(x) + "\n")
             res_file.write(temp_string)
 
-        kopje_2 = "\nReverse Primers\nSeq 5'-3',CG,AT,CG%,Smelt temp\n"
+        kopje_2 = "\nReverse Primers\nSeq 5'-3',CG,AT,CG%,Smelt temp,Length\n"
         res_file.write(kopje_2)
         for y in reverse_list:
             temp_string = str(",".join(y) + "\n")
@@ -132,15 +204,12 @@ def main():
 
     pre_seq_3, pre_seq_5 = res.raw_seq_3, res.raw_seq_5
 
-    seq_5 = seq_stove(pre_seq_5).upper()
-
     med_seq_3 = seq_stove(pre_seq_3).upper()
-    seq_3 = complementing_strand(med_seq_3)
+    three_utr_seq = complementing_strand(med_seq_3)
 
-    print(med_seq_3)
-    print(seq_3)
+    five_utr_seq = seq_stove(pre_seq_5).upper()
 
-    print(seq_5)
+    gui_2()
 
 
 main()
---
+++ testfile2.py
@@ -0,0 +1,77 @@
+from tkinter import *
+from tkinter.ttk import *
+
+
+def gui_2():
+
+    window = Tk()
+    window.title("Primer check 2: parameter")
+    window.geometry("250x175")
+
+    var_1, var_2, var_3, var_4 = IntVar(), IntVar(), IntVar(), IntVar()
+    var_1.set(60)
+    var_2.set(40)
+    var_3.set(55)
+    var_4.set(62)
+
+    label_maxcg = Label(window, text="Max CG%", font=("Arial", 12))
+    selector_maxcg = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_1, font=("Arial", 12))
+    label_maxcg.grid(column=0, row=0, sticky=W)
+    selector_maxcg.grid(column=0, row=1, sticky=W)
+    selector_maxcg.focus()
+
+    label_mincg = Label(window, text="Min CG%", font=("Arial", 12))
+    selector_mincg = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_2, font=("Arial", 12))
+    label_mincg.grid(column=0, row=2, sticky=W)
+    selector_mincg.grid(column=0, row=3, sticky=W)
+
+    label_maxtm = Label(window, text="Max Tm", font=("Arial", 12))
+    selector_maxtm = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_3, font=("Arial", 12))
+    label_maxtm.grid(column=0, row=4, sticky=W)
+    selector_maxtm.grid(column=0, row=5, sticky=W)
+
+    label_mintm = Label(window, text="Min Tm", font=("Arial", 12))
+    selector_mintm = Spinbox(window, from_=0, to=100, width=5,
+                             textvariable=var_4, font=("Arial", 12))
+    label_mintm.grid(column=0, row=6, sticky=W)
+    selector_mintm.grid(column=0, row=7, sticky=W)
+
+    label_maxlen = Label(window, text="Max primer length", font=("Arial", 12))
+    selector_maxlen = Spinbox(window, from_=0, to=100, width=5,
+                              textvariable=var_3, font=("Arial", 12))
+    label_maxlen.grid(column=0, row=4, sticky=W)
+    selector_maxlen.grid(column=0, row=5, sticky=W)
+
+    label_minlen = Label(window, text="Min primer length", font=("Arial", 12))
+    selector_minlen = Spinbox(window, from_=0, to=100, width=5,
+                              textvariable=var_4, font=("Arial", 12))
+    label_minlen.grid(column=0, row=6, sticky=W)
+    selector_minlen.grid(column=0, row=7, sticky=W)
+
+    continue_button = Button(window, text="Continue",
+                             command=lambda: window.quit())
+    continue_button.grid(column=2, row=7, sticky=E)
+
+    window.mainloop()
+
+    max_cg = selector_maxcg.get()
+    min_cg = selector_mincg.get()
+    max_tm = selector_maxtm.get()
+    min_tm = selector_mintm.get()
+    max_len = selector_maxlen.get()
+    min_len = selector_minlen.get()
+
+    return [max_cg, min_cg, max_tm, min_tm, max_len, min_len]
+
+
+def main():
+
+    para_list = list(gui_2())
+
+    print(para_list)
+
+
+main()
