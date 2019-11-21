ed6b1772a9a3bcb1e2da4c3212ee77fd37eeaaad
BramUt <bram.utberg@gmail.com>
2019-11-21 11:07:05 +0100

    Tried adding functional buttons

--- testfile.py
+++ testfile.py
@@ -7,10 +7,12 @@ def gui():
     def clicked_5():
         raw_seq_5 = txt_5.get("1.0", END)
         txt_5.configure(state='disabled', bg="light grey")
+        return raw_seq_5
 
     def clicked_3():
-        raw_seq_5 = txt_3.get("1.0", END)
+        raw_seq_3 = txt_3.get("1.0", END)
         txt_3.configure(state="disabled", bg="light grey")
+        return raw_seq_3
 
     window = Tk()
     window.title("Primer check 2")
@@ -18,7 +20,8 @@ def gui():
 
     label_5 = Label(window, text="Enter 5'-UTR")
     txt_5 = scrolledtext.ScrolledText(window, width=40, height=20)
-    button_5 = Button(window, text="OK", command=clicked_5)
+    button_5 = Button(window, text="OK",
+                      command=lambda: raw_seq_5 == clicked_5())
     label_5.grid(column=0, row=0, sticky=W)
     txt_5.grid(column=0, row=1)
     button_5.grid(column=0, row=2, sticky=E)
@@ -35,6 +38,9 @@ def gui():
     exit_button.grid(column=3, row=4, sticky=E)
 
     window.mainloop()
+
+    print(raw_seq_5)
+    print(raw_seq_3)
 
 
 gui()
